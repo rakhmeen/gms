7e7d8b7a593b7fd6833c7bffa2dc3aec06fa3c13
rakhmeen <88541672+rakhmeen@users.noreply.github.com>
2024-07-12 09:43:59 +0500

    Add files via upload
    updated

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from sqlalchemy.future import select
 from sqlalchemy import text
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-import os
 from DataBase import Base,engine
 
 app = FastAPI()
@@ -16,11 +15,12 @@ async def startup_event():
 async def init_db():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
-        with open("/docker-entrypoint-initdb.d/init.sql", "r") as file:
+        with open("init.sql", "r") as file:
             sql_statements = file.read().split(";")
             for statement in sql_statements:
                 if statement.strip():
                     await conn.execute(text(statement))
+
 
 @app.get("/")
 async def read_root():
